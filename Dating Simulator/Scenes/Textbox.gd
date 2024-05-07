50da0c267764bbe0f70a486444d20545f7678185
Crayonzla <98232931+Crayonzla@users.noreply.github.com>
2024-05-06 21:42:52 -0700

    Initial Commit

---
+++ Dating Simulator/Scenes/Textbox.gd
@@ -0,0 +1,51 @@
+extends Control
+
+var read_rate = 0.5
+var letter_index = 0
+
+var text
+
+@export var letter_time = 0.03
+
+@onready var label = $MarginContainer/Panel/MarginContainer/Label
+@onready var timer = $Timer
+
+
+func _ready():
+	_character_select()
+	_display_text()
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("Interact"):
+		label.set_text("")
+		Global.index += 1
+		letter_index = 0
+		_display_text()
+
+func _display_text():
+	if Global.index < text.size():
+		var current_text = text[Global.index]
+		if letter_index < current_text.length():
+			label.text += current_text[letter_index]
+			match current_text[letter_index]:
+				_:
+					timer.start(letter_time)
+			letter_index += 1
+		if Input.is_action_just_pressed("Force"):
+			label.set_text(text[Global.index])
+			letter_index = current_text.length()
+			return
+		else:
+			return
+	else:
+		return
+
+func _on_timer_timeout():
+	_display_text()
+
+func _character_select():
+	if Global.character == 0:
+		text = Dialog.text_1
+
+func _change_choices():
+	pass
---
+++ Dating Simulator/Singletons/Dialog.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var text_1= ["Text1",
+"Text2",
+"Text3", 
+"LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1LongText1",
+"End"
+]
